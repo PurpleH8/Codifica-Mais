45e48c1618d6e6a704685ed4dc6ebdcb7587c4af
PurpleH8 <kusuosaiki51@gmail.com>
2024-05-22 23:39:22 -0300

    mudanças e mais atividades sobre repetição, scrr

---
+++ PHP/exeteoricos/repeticao.php
@@ -0,0 +1,9 @@
+<?php
+
+$contador = 1;
+//while = enquanto
+//enquanto o $contador nao chegar a 15 {}
+while ($contador <= 15) {
+    echo "#$contador" . PHP_EOL;
+    $contador = $contador + 1; 
+}
---
+++ PHP/exeteoricos/repeticao2.php
@@ -0,0 +1,8 @@
+<?php
+$contador += 1;
+
+//para =for
+//para ($contador = 1; $contador <= 15; $contador + 1;) {
+for ($contador = 1; $contador <= 15 ; $contador += 1) {
+    echo "#$contador" . PHP_EOL;
+}
