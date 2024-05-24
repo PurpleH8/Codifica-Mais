c52e659af921d521a6269df8fad896aeea2ef5d8
PurpleH8 <kusuosaiki51@gmail.com>
2024-05-24 09:00:07 -0300

    desafios

--- PHP/desafios/desafio1.php
+++ PHP/desafios/desafio1.php
@@ -1,2 +1,8 @@
 <?php
 //executar um programa que exiba, na tela do usuário, todos os números ímpares de 0 à 100
+
+for ($contador = 1; $contador <10; $contador++) {
+    if ($contador % 2 != 0) {
+        echo $contador . PHP_EOL;
+    }
+}
