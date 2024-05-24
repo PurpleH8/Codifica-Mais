1eca57e451e6e9fe697356055a84370da6a475c2
PurpleH8 <kusuosaiki51@gmail.com>
2024-05-24 08:22:57 -0300

    nem sei mais

---
+++ PHP/desafios/desafio1.php
@@ -0,0 +1,2 @@
+<?php
+//executar um programa que exiba, na tela do usuário, todos os números ímpares de 0 à 100
--- PHP/exeteoricos/repeticao2.php
+++ PHP/exeteoricos/repeticao2.php
@@ -6,3 +6,24 @@ $contador += 1;
 for ($contador = 1; $contador <= 15 ; $contador += 1) {
     echo "#$contador" . PHP_EOL;
 }
+
+
+/* pular o numero 13
+
+for ($contador = 1; $contador <= 15; $contador ++) {
+    if ($contador == 13) {
+        continue;
+    } 
+    echo "#$contador" . PHP_EOL;
+}
+*/
+
+/* o "break" quebra o código, parando no numero 12 antes da condição original se concretizar
+
+for ($contador = 1; $contador <= 15; $contador ++) {
+    if ($contador == 13) {
+        break;
+    } 
+    echo "#$contador" . PHP_EOL;
+}
+ */
