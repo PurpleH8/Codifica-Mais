3dc389fe69d66bb42ba7c4a757291422865a7102
PurpleH8 <kusuosaiki51@gmail.com>
2024-05-31 02:42:05 -0300

    coisas

---
+++ PHP-CONCEITOS/exercicio2.php
@@ -0,0 +1,12 @@
+<?php 
+
+$quantidadeDeNotas = 3;
+
+$nota1 = 8;
+$nota2 = 5;
+$nota3 = 7;
+
+$notaSomada = $nota1 + $nota2 + $nota3;
+$media = $notaSomada / $quantidadeDeNotas;
+
+echo " a media das notas é $media";
--- PHP-CONCEITOS/exercicio3.php
+++ PHP-CONCEITOS/exercicio3.php
@@ -1,11 +1,11 @@
 <?php
 
-echo 'Diz a sua idade'. PHP_EOL;
+echo 'Diz a sua idade' . PHP_EOL;
 
-$idade = trim(fgets(STDIN));
+$idade = trim((fgets(STDIN)));
 
 if ($idade >= 18 ) {
     echo "$idade aninhos? pode entrar, trabalhador" . PHP_EOL;
 } else {
-    echo "$dade? novo dms, sai fora, menor";
+    echo "$idade? novo dms, sai fora, menor";
 }
