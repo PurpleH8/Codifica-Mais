2215954c1ab2a51c2f903393649aa813b8cbbb8f
PurpleH8 <kusuosaiki51@gmail.com>
2024-06-05 23:17:47 -0300

    easy peasy esse exercicio, da outra mais dificil (exercicio3 feito)

--- PHP-ARRAYS/php-b3/exercicio.php
+++ PHP-ARRAYS/php-b3/exercicio.php
@@ -1,6 +1,6 @@
 <?php
 
-/* crie um programa que calcule a área de um perímetro de um retângulo. O programa deve solicitar ao usuário a largura e a altura do retângulo, e então exibir os resultados*/
+/* crie um programa que calcule a área e o perímetro de um retângulo. O programa deve solicitar ao usuário a largura e a altura do retângulo, e então exibir os resultados*/
 /*formula: 
     área = largura * altura
     perímetro = 2 * (largura + altura)
@@ -12,4 +12,24 @@ exemplos:
     perímetro: 16 metros
 */
 
-echo 'olá, vamos calcular a área de um perímetro de um retângulo, mas para isso, preciso de você. Diga-me a largura do retângulo.' . PHP_EOL;
+echo "olá, vamos calcular a área e o perimetro de um retângulo, mas para isso preciso de você" . PHP_EOL;
+echo "DIga-me a largura do retângulo em metros" . PHP_EOL;
+
+
+$largura = fgets(STDIN);
+
+echo "Agora me diz a altura em metros." . PHP_EOL;
+
+$altura = fgets(STDIN);
+
+//calcular o perímetro
+
+$perimetro = 2 * ($largura + $altura);
+$area = $largura * $altura;
+
+echo "o perímetro do retângulo é $perimetro metros.". PHP_EOL;
+echo PHP_EOL;
+echo "a área do retângulo é $area metros quadrados." . PHP_EOL;
+echo PHP_EOL;
+echo "obrigado por utilizar esse programa amador, vá em paz!";
+
