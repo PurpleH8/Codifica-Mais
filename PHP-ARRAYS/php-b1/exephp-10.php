186e25ff0ad72c9fa7efaa38be6aa663347d11c0
PurpleH8 <kusuosaiki51@gmail.com>
2024-07-04 11:58:12 -0300

    exercicios parte1 feito (#2)

---
+++ PHP-ARRAYS/php-b1/exephp-10.php
@@ -0,0 +1,17 @@
+<?php
+
+echo 'Lerei um array de 5 elementos e imprimirei o array em ordem inversa' . PHP_EOL;
+
+$elemento = []; 
+
+for ($i = 0; $i < 5; $i++){
+    echo "Digite o " . ($i + 1) . "° elemento: ";
+    $elemento[] = trim(fgets(STDIN));
+}
+echo PHP_EOL;
+
+for ($i = 4; $i >= 0; $i--){
+    echo "O " . ($i + 1) . "º elemento da lista é: " . $elemento[$i] . PHP_EOL;
+}
+?>
+
