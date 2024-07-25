923cbf89b01d4a5f364d4a89f1db9f6075486e08
PurpleH8 <kusuosaiki51@gmail.com>
2024-07-25 20:54:17 -0300

    exercicio da aula presencial(eu tentei)

---
+++ Veiculos/src/Carro.php
@@ -0,0 +1,8 @@
+<?php
+
+class Carro extends Veiculos
+{
+    public function buzinar(){
+        "O carro " . $this->marca . $this->modelo . $this->cor . " está buzinando (BIBIIIIIIIIIIIIIIII BIBIBI BIIIIIIIII mds q idiota)";
+    }
+}
