6bb140ab2d7b5307435393a3a0ffc8e265083d70
PurpleH8 <kusuosaiki51@gmail.com>
2024-07-29 20:39:00 -0300

    Adicionando exercicio para fazer depois

---
+++ PHP-POO/Poo-02/exec.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+1. Crie uma interface chamada Animal. 
+2. A interface Animal deve ter um método chamado fazerSom. 
+3. Crie uma classe base Mamifero que implementa a interface Animal. 
+4. A classe Mamifero deve ter uma propriedade protegida chamada nome e 
+um método construtor que inicializa essa propriedade. 
+5. Crie uma classe Cachorro que herda de Mamifero e sobrescreva o método 
+fazerSom para retornar "Latido". 
+6. Crie uma classe Gato que herda de Mamifero e sobrescreva o método 
+fazerSom para retornar "Miau". 
+7. Crie uma função chamada fazerAnimalEmitirSom que aceita um objeto do 
+tipo Animal e chama o método fazerSom desse objeto. 
+8. Crie objetos das classes Cachorro e Gato e passe-os para a função 
+fazerAnimalEmitirSom. ]
+*/
