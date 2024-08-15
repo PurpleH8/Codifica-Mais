cfe1f33d7b1e2f765a2d69f5ad496f270481cd22
PurpleH8 <kusuosaiki51@gmail.com>
2024-08-15 20:58:12 -0300

    socorro html

---
+++ PROJETO/PROJETO/CRUD-WEB/public/listagem.php
@@ -0,0 +1,100 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<hea>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/listagem.css">
+    <title>Listagem de produtos</title>
+</head>
+<body>
+    <div class="tudo"> 
+        <div class="coluna_um">
+            <div class="coluna_cima_um">
+            
+                <div class="novo_item">
+
+                    NovoItem
+
+                </div>
+
+            </div>
+
+            <div class="coluna_cima_dois">
+
+                <div class="pesquisar">
+
+                    <div class="barra_pesquisar">
+                        <input type="text" placeholder="Buscar item" class="barra_pesquisar_original">
+                    </div>
+                </div>
+            </div>
+
+        
+        </div>
+
+        <div class="coluna_dois">
+            <div class="esquerdo_coluna_colunadois">
+                <div class="codigo_item_esquerda_coluna_colunadois">
+                    09121092841
+                </div>
+
+                <div class="tipo_item_esquerda_coluna_colunadois">
+                    tipo
+                </div>
+            </div>
+
+            <div class="direita_coluna">
+                <div class="codigo_item_direita_coluna">
+                    ajniauefnjkbis
+                </div>
+            </div>
+        </div>
+
+        <div class="coluna_tres">
+        <div class="esquerdo_coluna_colunatres">
+                <div class="codigo_item_esquerda_coluna_colunatres">
+                    09121092841
+                </div>
+
+                <div class="tipo_item_esquerda_coluna_colunatres">
+                    tipo
+                </div>
+            </div>
+
+            <div class="direita_coluna_colunatres">
+                <div class="codigo_item_direita_coluna_colunatres">
+                    ajniauefnjkbis
+                </div>
+            </div>
+        </div>
+
+        <div class="coluna_quatro">
+        <div class="esquerdo_coluna_colunaquatro">
+                <div class="codigo_item_esquerda_coluna_colunaquatro">
+                    09121092841
+                </div>
+
+                <div class="tipo_item_esquerda_coluna_colunaquatro">
+                    tipo
+                </div>
+            </div>
+
+            <div class="direita_coluna_colunaquatro">
+                <div class="codigo_item_direita_coluna_colunaquatro">
+                    ajniauefnjkbis
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    
+    <!-- Estrutura da página aqui -->
+
+</body>
+</html>
