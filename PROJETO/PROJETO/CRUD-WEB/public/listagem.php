9b9cd10da1cc4c433fd18defc7d3faedfe68724c
PurpleH8 <kusuosaiki51@gmail.com>
2024-08-19 20:46:28 -0300

    tentando fazer algo no dia 19/08

--- PROJETO/PROJETO/CRUD-WEB/public/listagem.php
+++ PROJETO/PROJETO/CRUD-WEB/public/listagem.php
@@ -1,4 +1,4 @@
-<?php
+<p?php
 session_start();
 
 ?>
@@ -39,31 +39,70 @@ session_start();
 
         <div class="coluna_dois">
             <div class="esquerdo_coluna_colunadois">
-                <div class="codigo_item_esquerda_coluna_colunadois">
-                    09121092841
+
+                <div class="top_esquerda_coluna_colunadois">
+                    
+                    <div class="codigo_item_esquerda_coluna_colunadois">
+                        <p>#000001</p>
+                    </div>
+
+                    <div class="tipo_item_esquerda_coluna_colunadois">
+                        <p>vestuário</p>
+                    </div>
+
+                </div>
+                
+                <div class="mid_esquerda_coluna_colunadois">
+                    <div class="produto_item_esquerda_coluna_colunadois">
+                        <p>Codifica+</p> 
+                    </div>
                 </div>
 
-                <div class="tipo_item_esquerda_coluna_colunadois">
-                    tipo
-                </div>
+                <button class="bot_esquerda_coluna_colunadois">
+                    
+                    
+                        Editar
+                    
+                    
+                </button>
+
             </div>
 
-            <div class="direita_coluna">
-                <div class="codigo_item_direita_coluna">
-                    ajniauefnjkbis
+            <div class="direita_coluna_colunadois">
+                <div class="top_direita_coluna_colunadois">   
+
+                    <div class="codigo_sku">
+                        SKU: 123456
+                    </div>
+
                 </div>
+
+                <div class="mid_direita_coluna_colunadois">
+                    Quantidade: 100
+                </div>
+
+                <button class="bot_direita_coluna_colunadois">
+                    deletar
+                </button>
+
             </div>
         </div>
 
         <div class="coluna_tres">
-        <div class="esquerdo_coluna_colunatres">
-                <div class="codigo_item_esquerda_coluna_colunatres">
-                    09121092841
+            <div class="esquerdo_coluna_colunatres">
+                <div class="top_esquerda_coluna_colunatres">
+                    
+                    <div class="codigo_item_esquerda_coluna_colunatres">
+                        <p>#000002</p>
+                    </div>
+                    
+                    <div class="tipo_item_esquerda_coluna_colunatres">
+                        <p>eletrônico</p>
+                    </div>
+
                 </div>
 
-                <div class="tipo_item_esquerda_coluna_colunatres">
-                    tipo
-                </div>
+                
             </div>
 
             <div class="direita_coluna_colunatres">
